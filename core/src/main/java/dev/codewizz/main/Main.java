7f4b881199d99587badbdb486a44102ff978b6c8
CodeWizz <tom.heijkamp@outlook.com>
2025-12-02 18:53:27 +0100

    models

--- core/src/main/java/dev/codewizz/main/Main.java
+++ core/src/main/java/dev/codewizz/main/Main.java
@@ -1,32 +1,57 @@
 package dev.codewizz.main;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+
+    public PerspectiveCamera cam;
+    public ModelBatch modelBatch;
+    public Model model;
+    public ModelInstance instance;
+    public Environment environment;
 
     @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+    public void create () {
+        modelBatch = new ModelBatch();
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f,
+                                       new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                                       VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        instance = new ModelInstance(model);
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
     }
 
     @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+    public void render () {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f, true);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
     }
 
     @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+    public void dispose () {
+        modelBatch.dispose();
+        model.dispose();
     }
 }
