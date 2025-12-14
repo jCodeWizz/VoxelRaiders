ee784d5247b3e857ae09884df7d5a0fc16e3da59
CodeWizz <tom.heijkamp@outlook.com>
2025-12-14 09:23:29 +0100

    environment

--- core/src/main/java/dev/codewizz/gfx/Renderer.java
+++ core/src/main/java/dev/codewizz/gfx/Renderer.java
@@ -1,7 +1,10 @@
 package dev.codewizz.gfx;
 
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.utils.ScreenUtils;
 import dev.codewizz.world.GameObject;
 import java.util.List;
@@ -10,10 +13,15 @@ public class Renderer {
 
     private final ModelBatch modelBatch;
     private final Camera camera;
+    private final Environment environment;
 
     public Renderer() {
         modelBatch = new ModelBatch();
         camera = new Camera();
+        environment = new Environment();
+
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
     }
 
     public void render(List<GameObject> objects) {
@@ -30,7 +38,7 @@ public class Renderer {
 
     public void renderObjectInstance(GameObject object, ModelInstance instance) {
         instance.transform.set(object.getPosition(), object.getRotation(), object.getScale());
-        modelBatch.render(instance);
+        modelBatch.render(instance, environment);
     }
 
     public void dispose() {
