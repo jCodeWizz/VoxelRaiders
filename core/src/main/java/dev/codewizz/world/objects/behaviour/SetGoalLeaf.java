05460e767628c8d3e1e387d41c8f514d6be8b743
Cody <tom.heijkamp@outlook.com>
2025-12-30 12:32:02 +0100

    fixed crash + better set goal leaf task

--- core/src/main/java/dev/codewizz/world/objects/behaviour/SetGoalLeaf.java
+++ core/src/main/java/dev/codewizz/world/objects/behaviour/SetGoalLeaf.java
@@ -2,6 +2,7 @@ package dev.codewizz.world.objects.behaviour;
 
 import com.badlogic.gdx.ai.btree.LeafTask;
 import com.badlogic.gdx.ai.btree.Task;
+import com.badlogic.gdx.math.MathUtils;
 import dev.codewizz.world.Entity;
 
 import dev.codewizz.world.World;
@@ -9,10 +10,16 @@ import java.util.Random;
 
 public class SetGoalLeaf extends LeafTask<Entity> {
 
+    private final static Random random = new Random();
+
     @Override
     public Status execute() {
         Entity e = getObject();
-        e.getAgent().setGoal(e.getAgent().getGraph().getCell(new Random().nextInt(World.SIZE), new Random().nextInt(World.SIZE)));
+
+        int navX = MathUtils.clamp(MathUtils.floor(e.getPosition().x + (World.SIZE/2f)) + random.nextInt(8) - 4, 0, World.SIZE-1);
+        int navY = MathUtils.clamp(MathUtils.floor(e.getPosition().z + (World.SIZE/2f)) + random.nextInt(8) - 4, 0, World.SIZE-1);
+
+        e.getAgent().setGoal(e.getAgent().getGraph().getCell(navX, navY));
         return Status.SUCCEEDED;
     }
 
