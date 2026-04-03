57dffb4ea75aa8250dddfa45dea6c9f1a8a91d0e
CodeWizz <tom.heijkamp@outlook.com>
2026-04-03 23:17:23 +0200

    hermit

--- core/src/main/java/dev/codewizz/world/objects/Hermit.java
+++ core/src/main/java/dev/codewizz/world/objects/Hermit.java
@@ -1,11 +1,24 @@
 package dev.codewizz.world.objects;
 
+import com.badlogic.gdx.ai.btree.BehaviorTree;
+import com.badlogic.gdx.ai.btree.decorator.Repeat;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import dev.codewizz.gfx.Renderer;
+import dev.codewizz.utils.Assets;
 import dev.codewizz.world.Entity;
 
 public class Hermit extends Entity {
 
     public Hermit() {
         super("vxr:hermit");
+
+        instance = new ModelInstance(Assets.findModel(getId())); //TODO: based on job
+
+        behaviour = new BehaviorTree<>(
+            new Repeat<>(
+                
+            ),
+            this
+        );
     }
 }
