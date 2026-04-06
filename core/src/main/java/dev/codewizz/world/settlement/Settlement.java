c8d7a6a6dd75899c38e1a0203a7123584d79a337
CodeWizz <tom.heijkamp@outlook.com>
2026-04-06 20:47:08 +0200

    stgarting settlement tasks

--- core/src/main/java/dev/codewizz/world/settlement/Settlement.java
+++ core/src/main/java/dev/codewizz/world/settlement/Settlement.java
@@ -1,8 +1,12 @@
 package dev.codewizz.world.settlement;
 
+import com.badlogic.gdx.ai.btree.BehaviorTree;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Queue;
+import dev.codewizz.world.Entity;
 import dev.codewizz.world.World;
 import dev.codewizz.world.objects.Hermit;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,11 +15,13 @@ public class Settlement {
     private final Vector3 position;
     private final World world;
 
+    private final Queue<BehaviorTree<Entity>> tasks;
     private final List<Hermit> members;
 
     public Settlement(World world, Vector3 position) {
         this.world = world;
         this.position = new Vector3(position);
+        tasks = new Queue<>();
         this.members = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
@@ -23,6 +29,14 @@ public class Settlement {
             hermit.getPosition().set(position.x, position.y + hermit.getSize().y / 2f, position.z);
             addMember(hermit);
         }
+    }
+
+    public Queue<BehaviorTree<Entity>> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(BehaviorTree<Entity> task) {
+        tasks.addLast(task);
     }
 
     public void addMember(Hermit hermit) {
