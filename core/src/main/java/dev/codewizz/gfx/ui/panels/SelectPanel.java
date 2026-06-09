81f050e7f7ab513a030d8e6fa54a32f52a30aa42
Cody <tom.heijkamp@outlook.com>
2026-06-09 12:39:19 +0200

    select menu  improvements

--- core/src/main/java/dev/codewizz/gfx/ui/panels/SelectPanel.java
+++ core/src/main/java/dev/codewizz/gfx/ui/panels/SelectPanel.java
@@ -1,23 +1,50 @@
 package dev.codewizz.gfx.ui.panels;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import dev.codewizz.gfx.ui.UI;
 import dev.codewizz.main.Main;
 import dev.codewizz.utils.Assets;
-import dev.codewizz.utils.Logger;
 import dev.codewizz.world.Entity;
 import dev.codewizz.world.GameObject;
 
 public class SelectPanel extends Panel {
+
+    private final static Environment ENVIRONMENT = new Environment();
+    private final static DirectionalLight DIRECTIONAL_LIGHT = new DirectionalLight().set(1, 1, 1, -1, -1, -1);
+    private final static PerspectiveCamera PERSPECTIVE_CAMERA = new PerspectiveCamera(45, 128, 128);
+    private final static FrameBuffer FBO = new FrameBuffer(Pixmap.Format.RGBA8888,128,128,true);
+    private final static ModelBatch MODEL_BATCH = new ModelBatch();
+    static {
+        ENVIRONMENT.add(DIRECTIONAL_LIGHT);
+
+        PERSPECTIVE_CAMERA.position.set(2, 2, 2);
+        PERSPECTIVE_CAMERA.lookAt(0, 1, 0);
+        PERSPECTIVE_CAMERA.near = 0.1f;
+        PERSPECTIVE_CAMERA.far = 100f;
+        PERSPECTIVE_CAMERA.update();
+    }
 
     private final GameObject target;
 
     private Label name;
     private Label description;
     private Label health;
+    private Image image;
 
     private Table buttons;
 
@@ -32,6 +59,7 @@ public class SelectPanel extends Panel {
             name.setText(target.getName());
             description.setText(target.getDescription());
             health.setText("Health");
+            image.setDrawable(new TextureRegionDrawable(createPreview(((Entity) target).getInstance())));
         } else {
             name.setText(target.getName());
             description.setText(target.getDescription());
@@ -53,7 +81,8 @@ public class SelectPanel extends Panel {
         description = new Label("", UI.skin);
         description.setWrap(true);
         health = new Label("", UI.skin);
-        Image image = new Image(Assets.findSprite("select-menu-head"));
+        image = new Image(Assets.findSprite("select-menu-head"));
+
 
         Table left = new Table();
         Table right = new Table();
@@ -68,5 +97,30 @@ public class SelectPanel extends Panel {
         buttons = new Table();
         content.add(right).expand().fill().right();
         content.add(buttons).size(22 * UI.SCALE, 54 * UI.SCALE).right();
+    }
+
+    public TextureRegion createPreview(ModelInstance inst) {
+        ModelInstance instance = new ModelInstance(inst);
+        instance.transform.idt();
+        instance.transform.rotate(Vector3.Y, 270);
+
+        FBO.begin();
+
+        Gdx.gl.glViewport(0, 0, 128, 128);
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        MODEL_BATCH.begin(PERSPECTIVE_CAMERA);
+        MODEL_BATCH.render(instance, ENVIRONMENT);
+        MODEL_BATCH.end();
+
+        FBO.end();
+
+        Texture texture = FBO.getColorBufferTexture();
+
+        TextureRegion region = new TextureRegion(texture);
+        region.flip(false, true);
+
+        return region;
     }
 }
